af14184cdfc616dcbc854fb6524aaed0f5d07dfb
Reyvin <reyvin.turvoiz@gmail.com>
2021-03-15 13:18:00 +0700

    penambahan table role dan user role

---
+++ database/migrations/2021_03_15_000001_create_table_role.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->bigIncrements('role_id')->nullable(false);
+            $table->string('role_name',50);
+            $table->string('role_type',50);            
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');          
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role');
+    }
+}
