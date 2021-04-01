4a2996679616c022f650ab447761deff18d2bc88
hkevinku <hkquaterback25@gmail.com>
2021-04-01 09:53:00 +0700

    Role by spaatyy

--- database/seeders/SpattyAssignRoleUser.php
+++ database/seeders/SpattyAssignRoleUser.php
@@ -12,14 +12,35 @@ class SpattyAssignRoleUser extends Seeder
     public function run()
     {
 
-        $user = User::where('email','kevin@bettersocial.org')->first();
+        $user = User::where('email','superadmin@bettersocial.org')->first();
         $user->assignRole('admin');
 
+        $user = User::where('email','viewer@bettersocial.org')->first();
+        $user->assignRole('viewer');
+
+
+        $user = User::where('email','bastian@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','aliirawan@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','dedesu@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','nyoman@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','budi@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','amril@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','putu@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','usup@bettersocial.org')->first();
+        $user->assignRole('editor');
+        $user = User::where('email','kevin@bettersocial.org')->first();
+        $user->assignRole('editor');
         $user = User::where('email','reyvin@bettersocial.org')->first();
         $user->assignRole('editor');
 
-        $user = User::where('email','nyoman@bettersocial.org')->first();
-        $user->assignRole('viewer');
+
 
 
     }
