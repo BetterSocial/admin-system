de2703b898354810ea71236c731c80c343b3f3c8
Reyvin <reyvin.turvoiz@gmail.com>
2021-04-07 13:33:27 +0700

    Penamabahan table domain news

---
+++ database/migrations/2021_04_07_050310_create_table_news_link.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableNewsLink extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_news_link', function (Blueprint $table) {
+            $table->bigIncrements("news_link_id");
+            $table->longText("news_url");
+            $table->bigInteger("domain_page_id");
+            $table->string("site_name",255)->nullable(true);
+            $table->string("title",255)->nullable(true);
+            $table->string("image",255)->nullable(true);
+            $table->longText("description")->nullable(true);
+            $table->longText("url")->nullable(true);
+            $table->string("keyword",255)->nullable(true);
+            $table->string("author",255)->nullable(true);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_news_link');
+    }
+}
