b20e8b2b3611ab7786298a4d5e493d8d02a3b08c
Reyvin <reyvin.turvoiz@gmail.com>
2021-05-04 15:50:18 +0700

    commit test unit user score

--- tests/Feature/UserScoreTest.php
+++ tests/Feature/UserScoreTest.php
@@ -20,7 +20,9 @@ class UserScoreTest extends TestCase
     {
         $formula = new FormulaController();
         $w_y = config('constants.weight_follower_quality');
-        $u = $formula->UserScore(0, 1.85, $w_y);
+        $u1 = 0;
+        $y = 1.85;
+        $u = $formula->UserScore($u1, $y, $w_y);
 
         $this->assertEquals(0, $u);
     }
@@ -28,7 +30,9 @@ class UserScoreTest extends TestCase
     public function test_2() {
         $formula = new FormulaController();
         $w_y = config('constants.weight_follower_quality');
-        $u = $formula->UserScore(0.727, 1.98, $w_y);
+        $u1 = 0.727;
+        $y = 1.98;
+        $u = $formula->UserScore($u1, $y, $w_y);
 
         $this->assertEquals(1.43946, $u);
     }
@@ -36,7 +40,9 @@ class UserScoreTest extends TestCase
     public function test_3() {
         $formula = new FormulaController();
         $w_y = config('constants.weight_follower_quality');
-        $u = $formula->UserScore(1.1, 1.73, $w_y);
+        $u1 = 1.1;
+        $y = 1.73;
+        $u = $formula->UserScore($u1, $y, $w_y);
 
         $this->assertEquals(1.903, $u);
     }
@@ -44,7 +50,9 @@ class UserScoreTest extends TestCase
     public function test_4() {
         $formula = new FormulaController();
         $w_y = config('constants.weight_follower_quality');
-        $u = $formula->UserScore(2.5, 1.85, $w_y);
+        $u1 = 2.5;
+        $y = 1.85;
+        $u = $formula->UserScore($u1, $y, $w_y);
 
         $this->assertEquals(4.625, $u);
     }
@@ -52,9 +60,74 @@ class UserScoreTest extends TestCase
     public function test_5() {
         $formula = new FormulaController();
         $w_y = config('constants.weight_follower_quality');
-        $u = $formula->UserScore(100, 1.69, $w_y);
+        $u1 = 100;
+        $y = 1.69;
+        $u = $formula->UserScore($u1, $y, $w_y);
 
         $this->assertEquals(169, $u);
+    }
+    public function test_6() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 1225;
+        $y = 1.51;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(1849.75, $u);
+    }
+    public function test_7() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 2500;
+        $y = 1.73;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(4325, $u);
+    }
+    public function test_8() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 3249;
+        $y = 1.68;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(5458.32, $u);
+    }
+    public function test_9() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 3636;
+        $y = 1.86;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(6762.96, $u);
+    }
+    public function test_10() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 5105;
+        $y = 1.98;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(10107.9, $u);
+    }
+    public function test_11() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 8181;
+        $y = 1.85;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(15134.85, $u);
+    }
+    public function test_12() {
+        $formula = new FormulaController();
+        $w_y = config('constants.weight_follower_quality');
+        $u1 = 10000;
+        $y = 1.98;
+        $u = $formula->UserScore($u1, $y, $w_y);
+
+        $this->assertEquals(19800, $u);
     }
 
 }
