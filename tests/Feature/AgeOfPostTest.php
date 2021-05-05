5bc2f1fa9aa180c50b970edfb2ff37c295a62e27
hkevinku <hkquaterback25@gmail.com>
2021-05-05 11:20:18 +0700

    AgeOfPostTest AveragePostScoreTest finished

--- tests/Feature/AgeOfPostTest.php
+++ tests/Feature/AgeOfPostTest.php
@@ -19,81 +19,92 @@ class AgeOfPostTest extends TestCase
     public function test_1()
     {
         $formula = new FormulaController();
+        $expiration_setting = 1;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "4/28/2021 9:00:00";
-        $post_time = $formula->AgeOfPost(1, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(1, $post_time);
     }
 
     public function test_2(){
         $formula = new FormulaController();
+        $expiration_setting = 1;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "4/28/2021 9:00:00";
-        $post_time = $formula->AgeOfPost(1, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(1, $post_time);
     }
+
     public function test_3(){
         $formula = new FormulaController();
+        $expiration_setting = 1;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "4/29/2021 13:00:00";
-        $post_time = $formula->AgeOfPost(1, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(1, $post_time);
     }
 
     public function test_4(){
         $formula = new FormulaController();
+        $expiration_setting = 1;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "4/30/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(1, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(1, $post_time);
     }
 
     public function test_5(){
         $formula = new FormulaController();
+        $expiration_setting = 7;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "4/28/2021 19:00:00";
-        $post_time = $formula->AgeOfPost(7, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(1, $post_time);
     }
 
     public function test_6(){
         $formula = new FormulaController();
+        $expiration_setting = 7;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "5/3/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(7, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(5, $post_time);
     }
 
     public function test_7(){
         $formula = new FormulaController();
+        $expiration_setting = 7;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "5/7/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(7, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(7, $post_time);
     }
 
     public function test_8(){
         $formula = new FormulaController();
+        $expiration_setting = 30;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "5/27/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(30, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(29, $post_time);
     }
 
     public function test_9(){
         $formula = new FormulaController();
+        $expiration_setting = 30;
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "5/29/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(30, $post_datetime, $now_datetime);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
         $this->assertEquals(30, $post_time);
     }
 
     public function test_10(){
         $formula = new FormulaController();
+        $expiration_setting = 'forever';
         $post_datetime = "4/28/2021 7:00:00";
         $now_datetime = "5/31/2021 7:00:00";
-        $post_time = $formula->AgeOfPost(1, $post_datetime, $now_datetime);
-        $this->assertEquals(1, $post_time);
+        $post_time = $formula->AgeOfPost($expiration_setting, $post_datetime, $now_datetime);
+        $this->assertEquals(33, $post_time);
     }
 
 }
--- tests/Feature/AveragePostScoreTest.php
+++ tests/Feature/AveragePostScoreTest.php
@@ -19,41 +19,81 @@ class AveragePostScoreTest extends TestCase
     public function test_1()
     {
         $formula = new FormulaController();
-        $post_score = $formula->AveragePostScore(0,0);
+        $post_performance_score = 0;
+        $count_post = 0;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
         $this->assertEquals(1, $post_score);
     }
 
     public function test_2(){
         $formula = new FormulaController();
-        $post_score = $formula->AveragePostScore(0,10);
+        $post_performance_score = 0;
+        $count_post = 10;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
         $this->assertEquals(0, $post_score);
     }
 
     public function test_3(){
         $formula = new FormulaController();
-        $post_score = $formula->AveragePostScore(10,0);
+        $post_performance_score = 10;
+        $count_post = 0;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
         $this->assertEquals(2, $post_score);
     }
 
     public function test_4(){
         $formula = new FormulaController();
-        $post_score = $formula->AveragePostScore(25,5);
-        $this->assertEquals(2, $post_score);
+        $post_performance_score = 25;
+        $count_post = 5;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(3, $post_score);
     }
 
     public function test_5(){
         $formula = new FormulaController();
-        $post_score = $formula->AveragePostScore(100,20);
+        $post_performance_score = 100;
+        $count_post = 20;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
         $this->assertEquals(10, $post_score);
     }
 
-    public function test_6(){}
+    public function test_6(){
+        $formula = new FormulaController();
+        $post_performance_score = 500;
+        $count_post = 50;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(50, $post_score);
+    }
 
-    public function test_7(){}
+    public function test_7(){
+        $formula = new FormulaController();
+        $post_performance_score = 60;
+        $count_post = 55;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(6, $post_score);
+    }
 
-    public function test_8(){}
+    public function test_8(){
+        $formula = new FormulaController();
+        $post_performance_score = 25000;
+        $count_post = 100;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(2500, $post_score);
+    }
 
-    public function test_9(){}
+    public function test_9(){
+        $formula = new FormulaController();
+        $post_performance_score = 1125;
+        $count_post = 1000;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(112.5, $post_score);
+    }
 
-    public function test_10(){}
+    public function test_10(){
+        $formula = new FormulaController();
+        $post_performance_score = 0;
+        $count_post = 1;
+        $post_score = $formula->AveragePostScore($post_performance_score,$count_post);
+        $this->assertEquals(0.9, $post_score);
+    }
 }
