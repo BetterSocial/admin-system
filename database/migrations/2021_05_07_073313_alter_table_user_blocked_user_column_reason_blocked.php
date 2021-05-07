50cd772adfebc7082078f365053f65ff8bdd83ac
hkevinku <hkquaterback25@gmail.com>
2021-05-07 14:39:14 +0700

    perbaikan nullable pada kolom reason_blocked table user_blocked_user

---
+++ database/migrations/2021_05_07_073313_alter_table_user_blocked_user_column_reason_blocked.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableUserBlockedUserColumnReasonBlocked extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE user_blocked_user ALTER COLUMN reason_blocked DROP NOT NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
