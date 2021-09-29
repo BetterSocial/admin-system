0c8cf437c3509ad2c79e9a059a43dd1eac4b5071
Muhammad Fajariansyah Ismail <fajariansyah.ismail@gmail.com>
2021-09-29 20:46:17 +0700

    Add vote comment migration

---
+++ database/migrations/2021_09_29_132539_create_vote_comments.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('vote_comments', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+        Schema::create('vote_comments', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('comment_id');
+            $table->uuid('user_id');
+            $table->string('table');
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE vote_comments ALTER COLUMN id SET DEFAULT uuid_generate_v4 ()');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote_comments');
+    }
+}
