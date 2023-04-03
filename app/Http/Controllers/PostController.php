7097b4f48accfa19cb80be206e00cb0acdfbb6c4
usupsuparma <usup.suparma7@gmail.com>
2023-04-03 07:08:26 +0700

    fix: create post from csv

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ApiKey;
 use Illuminate\Http\Request;
 use GetStream\Stream\Client;
 use Illuminate\Support\Facades\Http;
@@ -29,74 +30,91 @@ class PostController extends Controller
     public function upload(Request $request)
     {
 
-        $request->validate([
-            'csv_file' => 'required|mimes:csv,txt'
-        ]);
-        if ($request->hasFile('csv_file')) {
-            $file = $request->file('csv_file')->getRealPath();
-
-            $csv = Reader::createFromPath($file, 'r');
-            $csv->setHeaderOffset(0); // jika CSV file memiliki header, atur offsetnya
-
-            $data = [];
-
-            foreach ($csv as $record) {
-                // lakukan sesuatu dengan setiap baris record di sini
-                // contoh: $record['column_name']
-                $userId = $record['user_id'];
-                $anonimity = $record['anonimity'];
-                $durationFeed = $record['duration_feed'];
-                $feedGroup = $record['feed_group'];
-                $location = $record['location'];
-                $locationId = $record['location_id'];
-                $message = $record['message'];
-                $object = $record['object'];
-                $privacy = $record['privacy'];
-                $images_url = $record['images_url'];
-                $verb = $record['verb'];
-                $topics = $record['topics'];
-                $itemTopics = explode(",", $topics);
-
-                // foreach ($itemTopics as $item) {
-                //     dd($item);
-                // }
-
-
-                $post = [
-                    'userId' => $userId,
-                    'anonimity' => $anonimity,
-                    "duration_feed" => $durationFeed,
-                    "feedGroup" => $feedGroup,
-                    "location" => $location,
-                    "location_id" => $locationId,
-                    "message" => $message,
-                    "object" => $object,
-                    "privacy" => $privacy,
-                    "images_url" => $images_url,
-                    "topics" => $itemTopics,
-                    "verb" => $verb
-                ];
-                $data[] = $post;
-            }
-
-            $url = config('constants.user_api');
-            $baseUrl = $url . '/api/v1/admin/bulk-post';
-            $response = Http::send('POST', $baseUrl, [
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                ],
-                'json' => $data,
+        try {
+            //code...
+            $request->validate([
+                'csv_file' => 'required|mimes:csv,txt'
             ]);
+            if ($request->hasFile('csv_file')) {
+                $file = $request->file('csv_file')->getRealPath();
 
-            if ($response->ok()) {
-                // handling jika request berhasil
-                $data = $response->json();
-                return $this->successResponseWithAlert('success', $data);
-            } else {
-                // handling jika request gagal
-                $status = $response->status();
-                return $this->errorResponseWithAlert($status);
+                $csv = Reader::createFromPath($file, 'r');
+                $csv->setHeaderOffset(0); // jika CSV file memiliki header, atur offsetnya
+
+                $data = [];
+
+                foreach ($csv as $record) {
+                    // lakukan sesuatu dengan setiap baris record di sini
+                    // contoh: $record['column_name']
+                    $userId = $record['user_id'];
+                    $anonimity = $record['anonimity'];
+                    $durationFeed = $record['duration_feed'];
+                    $feedGroup = $record['feed_group'];
+                    $location = $record['location'];
+                    $locationId = $record['location_id'];
+                    $message = $record['message'];
+                    $object = $record['object'];
+                    $privacy = $record['privacy'];
+                    $images_url = $record['images_url'];
+                    $verb = $record['verb'];
+                    $topics = $record['topics'];
+                    $itemTopics = explode(",", $topics);
+
+                    // foreach ($itemTopics as $item) {
+                    //     dd($item);
+                    // }
+
+
+                    $post = [
+                        'userId' => $userId,
+                        'anonimity' => $anonimity,
+                        "duration_feed" => $durationFeed,
+                        "feedGroup" => $feedGroup,
+                        "location" => $location,
+                        "location_id" => $locationId,
+                        "message" => $message,
+                        "object" => $object,
+                        "privacy" => $privacy,
+                        "images_url" => $images_url,
+                        "topics" => $itemTopics,
+                        "verb" => $verb
+                    ];
+                    $data[] = $post;
+                }
+
+                $apiKey = ApiKey::latest()->first();
+
+                if (!$apiKey) {
+                    $apiKey = ApiKey::create([
+                        'key' => $this->generateApiKey()
+                    ]);
+                }
+
+                $url = config('constants.user_api');
+                $baseUrl = $url . '/api/v1/admin/bulk-post';
+                $response = Http::send('POST', $baseUrl, [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'api-key' => $apiKey->key,
+                    ],
+                    'json' => ['post' => $data],
+                ]);
+
+                if ($response->ok()) {
+                    // handling jika request berhasil
+                    $data = $response->json();
+                    return $this->successResponseWithAlert('success created post');
+                } else {
+                    // handling jika request gagal
+                    $status = $response->status();
+                    $data = $response->json();
+                    return $this->errorResponse('failed created post');
+                }
             }
+        } catch (\Throwable $th) {
+            //throw $th;
+            dd($th);
+            return $this->errorResponseWithAlert($th->getMessage());
         }
     }
 
@@ -132,5 +150,16 @@ class PostController extends Controller
         } catch (\Throwable $th) {
             return $this->errorResponse('error delete comment ' . $th->getMessage());
         }
+    }
+
+    private function generateApiKey()
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $apiKey = '';
+        for ($i = 0; $i < 10; $i++) {
+            $apiKey .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $apiKey;
     }
 }
