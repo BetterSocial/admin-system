2950af190c62436374bda9ea6bdbcd9b7c9d30c2
falhar <firman.ali39@gmail.com>
2023-06-13 23:55:42 +0700

    fix: add column priority

---
+++ database/migrations/2023_06_13_154810_add_column_priority_to_rss_links.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnPriorityToRssLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rss_links', function (Blueprint $table) {
+            $table->integer('priority')->after('link')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rss_links', function (Blueprint $table) {
+            $table->dropColumn('priority');
+        });
+    }
+}
