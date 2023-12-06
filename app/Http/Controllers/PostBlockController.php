e58511abeaf4c49b00f499421966cf2a13f38f38
usupsuparma <usup.suparma7@gmail.com>
2023-12-07 00:27:35 +0700

    fix: add search by ID

--- app/Http/Controllers/PostBlockController.php
+++ app/Http/Controllers/PostBlockController.php
@@ -65,7 +65,9 @@ class PostBlockController extends Controller
             $activityIds = [];
             $dataTable = dataTableRequestHandle($req);
             if ($message) {
+                // make search post_id and post content
                 $posts = PostModel::where('post_content', 'ilike', '%' . $message . '%')
+                    ->orWhere('post_id', 'ilike', '%' . $message . '%')
                     ->whereNotNull('getstream_activity_id')
                     ->get()
                     ->pluck('getstream_activity_id');
