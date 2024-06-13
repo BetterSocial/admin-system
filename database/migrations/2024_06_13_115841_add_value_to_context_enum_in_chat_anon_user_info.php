5f8cec3c4556c14fe48285757a9f49b90967e572
n-permana <permana0415@gmail.com>
2024-06-13 21:55:53 +0700

    feat: change alter enum script migration

--- database/migrations/2024_06_13_115841_add_value_to_context_enum_in_chat_anon_user_info.php
+++ database/migrations/2024_06_13_115841_add_value_to_context_enum_in_chat_anon_user_info.php
@@ -13,7 +13,12 @@ class AddValueToContextEnumInChatAnonUserInfo extends Migration
      */
     public function up()
     {
-        DB::statement("ALTER TABLE chat_anon_user_info MODIFY COLUMN context ENUM('other profile', 'chat', 'post', 'follow', 'auto message')");
+         // Create the ENUM type
+         DB::statement("CREATE TYPE context_enum AS ENUM ('other profile', 'chat', 'post', 'follow', 'auto message')");
+
+         // Change the column type to the new ENUM type
+         DB::statement("ALTER TABLE chat_anon_user_info ALTER COLUMN context TYPE context_enum USING context::text::context_enum");
+
     }
 
     /**
@@ -23,6 +28,10 @@ class AddValueToContextEnumInChatAnonUserInfo extends Migration
      */
     public function down()
     {
-        DB::statement("ALTER TABLE chat_anon_user_info MODIFY COLUMN context ENUM('other profile', 'chat', 'post', 'follow')");
+        // Create the ENUM type
+        DB::statement("CREATE TYPE context_enum AS ENUM ('other profile', 'chat', 'post', 'follow')");
+
+        // Change the column type to the new ENUM type
+        DB::statement("ALTER TABLE chat_anon_user_info ALTER COLUMN context TYPE context_enum USING context::text::context_enum");
     }
 }
